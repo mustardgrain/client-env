e69df9a785d2cb9cc8a9c1c4cbf9ee084118b0f3
Kirk True <kirk@mustardgrain.com>
2016-01-13 11:32:17 -0800

    Using local Bash variables

--- .client-env-completion.bash
+++ .client-env-completion.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 function __client-env-symlink-add() {
-  client=$1
-  dir=$2
-  file=$3
+  local client=$1
+  local dir=$2
+  local file=$3
 
   if [ -f $dir/$file ] ; then
     if [ ! -L "$dir/$file" ] ; then
@@ -12,7 +12,7 @@ function __client-env-symlink-add() {
     fi
   fi
 
-  orig_pwd=`pwd`
+  local orig_pwd="`pwd`"
   mkdir -p $dir
   cd $dir
   rm -f $file
@@ -21,30 +21,30 @@ function __client-env-symlink-add() {
 }
 
 function __client-env-symlink-rm() {
-  client=$1
-  dir=$2
-  file=$3
+  local client=$1
+  local dir=$2
+  local file=$3
 
   if [ ! -L "$dir/$file" ] ; then
     echo "Not removing $dir/$file as it is not a symbolic link"
     return 1
   fi
 
-  orig_pwd=`pwd`
+  local orig_pwd="`pwd`"
   cd $dir
   rm -f $file
   cd "$orig_pwd"
 }
 
 function client-env-set() {
-  client=$1
+  local client=$1
 
   if [ "$client" = "" ] ; then
     echo "Please specify the client environment"
     return 1
   fi
 
-  orig_pwd=`pwd`
+  local orig_pwd="`pwd`"
 
   if [ -d "$LOCAL_DEV_DIR/$client" ] ; then
     cd $LOCAL_DEV_DIR/$client
@@ -77,8 +77,8 @@ function client-env-set() {
 
 function client-env-clear() {
   if [ -L "$HOME/.current-client-env" ] ; then
-    client=$(basename `readlink $HOME/.current-client-env`)
-    orig_pwd=`pwd`
+    local client=$(basename `readlink $HOME/.current-client-env`)
+    local orig_pwd="`pwd`"
 
     if [ -f "$LOCAL_DEV_DIR/$client/$client-env/conf/client-env-clear.sh" ] ; then
       source $LOCAL_DEV_DIR/$client/$client-env/conf/client-env-clear.sh
@@ -93,7 +93,7 @@ function client-env-clear() {
 
 function _client_env_set_completion() {
   # Look at the list of directories in $LOCAL_DEV_DIR
-  orig_pwd=`pwd`
+  local orig_pwd="`pwd`"
 
   cd "$LOCAL_DEV_DIR"
 
