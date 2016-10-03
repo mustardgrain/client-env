308438ded1187e1e6fbf07c9964a4194aeac81a1
Kirk True <kirk@mustardgrain.com>
2016-10-03 08:03:40 -0700

    Added NTP workaround for Dockers and related disclaimers

--- .client-env-completion.bash
+++ .client-env-completion.bash
@@ -3,6 +3,11 @@
 function __enable_docker() {
   local client=$1
 
+  echo "--------------------------------------------------------------------------------"
+  echo "WARNING: Implementing temporary workaround for stability issues running Docker on macOS Sierra"
+  sudo launchctl unload /System/Library/LaunchDaemons/org.ntp.ntpd.plist
+  echo "--------------------------------------------------------------------------------"
+
   if [ "$client" = "" ] ; then
     echo "Please specify the client environment"
     return 1
@@ -24,6 +29,11 @@ function __enable_docker() {
 }
 
 function __disable_docker() {
+  echo "--------------------------------------------------------------------------------"
+  echo "WARNING: Implementing temporary workaround for stability issues running Docker on macOS Sierra"
+  sudo launchctl unload /System/Library/LaunchDaemons/org.ntp.ntpd.plist
+  echo "--------------------------------------------------------------------------------"
+
   if [ `uname` = 'Darwin' -a "`which docker-machine`" != "" ] ; then
     # Auto-stop the Docker machine, if needed.
     if [ "`docker-machine status $DOCKER_MACHINE_VM_NAME`" = 'Running' ] ; then
