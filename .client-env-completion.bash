0aaadfc11d9f42fbf3a7d836af653831b36363d5
Kirk True <kirk@mustardgrain.com>
2016-12-19 16:51:15 -0800

    Simplified the function calls by inferring the client ID, added ability to merge/unmerge SSH files

--- .client-env-completion.bash
+++ .client-env-completion.bash
@@ -1,9 +1,42 @@
 #!/bin/bash
 
+if [ "$MASTER_SSH_CONFIG" = "" ] ; then
+  echo "Please set the MASTER_SSH_CONFIG environment variable before using client-env"
+  return 2
+fi
+
+if [ "$LOCAL_DEV_DIR" = "" ] ; then
+  echo "Please set the LOCAL_DEV_DIR environment variable before using client-env"
+  return 2
+fi
+
+function __client-env-get-client() {
+  if [ -L "$HOME/.current-client-env" ] ; then
+    echo $(basename `readlink $HOME/.current-client-env`)
+    return 0
+  else
+    echo "Please make sure to have called client-env-set before using client-env functions"
+    return 3
+  fi
+}
+
+function __client-env-ssh-config-merge() {
+  # Merge our SSH config templates together for the client
+  local client=`__client-env-get-client`
+
+  cat $MASTER_SSH_CONFIG                                    > $HOME/.ssh/config
+  cat $LOCAL_DEV_DIR/$client/${client}-env/conf/ssh_config >> $HOME/.ssh/config
+}
+
+function __client-env-ssh-config-revert() {
+  # Copy our original config template back
+  cp $MASTER_SSH_CONFIG $HOME/.ssh/config
+}
+
 function __client-env-cp() {
-  local client=$1
-  local dst_dir=$2
-  local file_name=$3
+  local client=`__client-env-get-client`
+  local dst_dir=$1
+  local file_name=$2
   local dst_file_name=$dst_dir/$file_name
   local src_file_name=$LOCAL_DEV_DIR/$client/${client}-env/conf/$file_name
 
@@ -17,9 +50,9 @@ function __client-env-cp() {
 }
 
 function __client-env-rm() {
-  local client=$1
-  local dst_dir=$2
-  local file_name=$3
+  local client=`__client-env-get-client`
+  local dst_dir=$1
+  local file_name=$2
   local dst_file_name=$dst_dir/$file_name
   local src_file_name=$LOCAL_DEV_DIR/$client/${client}-env/conf/$file_name
 
@@ -32,9 +65,9 @@ function __client-env-rm() {
 }
 
 function __client-env-symlink-add() {
-  local client=$1
-  local dst_dir=$2
-  local file_name=$3
+  local client=`__client-env-get-client`
+  local dst_dir=$1
+  local file_name=$2
   local dst_file_name=$dst_dir/$file_name
   local src_file_name=$LOCAL_DEV_DIR/$client/${client}-env/conf/$file_name
 
@@ -48,9 +81,9 @@ function __client-env-symlink-add() {
 }
 
 function __client-env-symlink-rm() {
-  local client=$1
-  local dst_dir=$2
-  local file_name=$3
+  local client=`__client-env-get-client`
+  local dst_dir=$1
+  local file_name=$2
   local dst_file_name=$dst_dir/$file_name
 
   if [ ! -L "$dst_file_name" ] ; then
@@ -69,32 +102,32 @@ function client-env-set() {
     return 1
   fi
 
+  if [ ! -d "$LOCAL_DEV_DIR/$client" ] ; then
+    echo "Could not find $client in $LOCAL_DEV_DIR"
+    return
+  fi
+
   local orig_pwd="`pwd`"
 
-  if [ -d "$LOCAL_DEV_DIR/$client" ] ; then
+  cd $LOCAL_DEV_DIR/$client
+
+  if [ -d "$client-env/bin" ] ; then
+    cd $client-env/bin
+    export PATH="`pwd -P`:$PATH"
     cd $LOCAL_DEV_DIR/$client
+  fi
 
-    if [ -d "$client-env/bin" ] ; then
-      cd $client-env/bin
-      export PATH="`pwd -P`:$PATH"
-      cd ../..
-    fi
+  PS1="($client) $PS1"
 
-    if [ -f "$client-env/conf/client-env-set.sh" ] ; then
-      source $client-env/conf/client-env-set.sh
-    fi
+  if [ ! -L "$HOME/.current-client-env" ] ; then
+    cd $HOME
+    ln -s $LOCAL_DEV_DIR/$client .current-client-env
+    cd $LOCAL_DEV_DIR/$client
+  fi
 
-    PS1="($client) $PS1"
-
-    if [ ! -L "$HOME/.current-client-env" ] ; then
-      # Make our current client setting sticky. Remember to go back
-      # to the LOCAL_DEV_DIR directory because the above source
-      # could leave us in an arbitrary directory :\
-      cd $HOME
-      ln -s $LOCAL_DEV_DIR/$client .current-client-env
-    fi
-  else
-    echo "Could not find $client in $LOCAL_DEV_DIR"
+  if [ -f "$client-env/conf/client-env-set.sh" ] ; then
+    source $client-env/conf/client-env-set.sh
+    cd $LOCAL_DEV_DIR/$client
   fi
 
   cd "$orig_pwd"
